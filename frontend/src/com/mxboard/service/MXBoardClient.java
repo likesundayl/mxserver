04d1008401dcae054804826c715f8ad0878348c6
TerenceWu <terencewuzhenan@126.com>
2017-12-24 22:43:52 +0800

    Add mxboard_server.py and MXBoardClient.java

---
+++ frontend/src/com/mxboard/service/MXBoardClient.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2017 Terence Wu
+ */
+package com.mxboard.service;
+
+/**
+ * <tt>MXBoardClient.java</tt><br>
+ * 
+ * @author TerenceWu
+ * @time 2017年12月24日 下午10:42:21
+ * @version 1.0
+ */
+public class MXBoardClient {
+
+}
