dcc3fa1791d2800c82d364b73f8fcfb8b4e88f86
TerenceWu <terencewuzhenan@126.com>
2017-12-25 11:36:28 +0800

    Add logger_generator.py

---
+++ backend/mxboard/log/logger_generator.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# ------------------------------
+# Copyright (c) 2017 Terence Wu
+# ------------------------------
+import time
+import logging
+import logging.handlers as log_handlers
+
+from backend.mxboard.util.xml_parser import mxboard_log_config
+
+
+current_date = time.strftime('%Y-%m-%d', time.localtime())
+log_file = '../../../log/' + current_date + '-log.txt'
+
+
+def get_logger(logger_name):
+    handler = log_handlers.RotatingFileHandler(filename=log_file,
+                                               maxBytes=long(mxboard_log_config['log-max-bytes']),
+                                               backupCount=int(mxboard_log_config['log-backup-count']))
+    formatter = logging.Formatter(mxboard_log_config['log-format'])
+    handler.setFormatter(formatter)
+    logger = logging.getLogger(logger_name)
+    logger.addHandler(handler)
+    level = mxboard_log_config['log-level']
+    if level == 'INFO':
+        logger.setLevel(logging.INFO)
+    elif level == 'DEBUG':
+        logger.setLevel(logging.DEBUG)
+
+    return logger
