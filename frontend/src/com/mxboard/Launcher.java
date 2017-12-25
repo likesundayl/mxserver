64155aeb78a13cc965efcb110ebacdde526c4190
TerenceWu <terencewuzhenan@126.com>
2017-12-25 17:06:12 +0800

    Add Launcher.java

---
+++ frontend/src/com/mxboard/Launcher.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2017 Terence Wu
+ */
+package com.mxboard;
+
+/**
+ * <tt>Launcher.java</tt><br>
+ * 
+ * @author TerenceWu
+ * @time 2017年12月25日 下午5:04:54
+ * @version 1.0
+ */
+public class Launcher {
+	public void launch() {
+		
+	}
+	
+	public static void main(String[] args) {
+		Launcher launcher = new Launcher();
+		launcher.launch();
+	}
+}
