1a4a900a876a53972b65e301add55568412f4868
TerenceWu <terencewuzhenan@126.com>
2017-12-25 10:38:02 +0800

    Update XMLConfigParser.java
    1. Add 2 methods

--- frontend/src/com/mxboard/util/XMLConfigParser.java
+++ frontend/src/com/mxboard/util/XMLConfigParser.java
@@ -3,6 +3,15 @@
  */
 package com.mxboard.util;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
 /**
  * <tt>XMLConfigParser.java</tt><br>
  * 
@@ -19,5 +28,58 @@ public class XMLConfigParser {
 	static {
 		int lastSepIndex = XML_PATH.lastIndexOf(FILE_SEP);
 		XML_PATH = XML_PATH.substring(0, lastSepIndex + 1) + FOLDER_NAME + FILE_SEP + XML_NAME;
+	}
+	
+	private Element rootElement;
+	
+	public XMLConfigParser() {
+		SAXReader saxReader = new SAXReader();
+		try {
+			Document document = saxReader.read(new File(XML_PATH));
+			rootElement = document.getRootElement();
+		} catch (DocumentException e) {
+		}
+	}
+	
+	public HashMap<String, String> getMongoConfig() {
+		HashMap<String, String> mongoConfig = new HashMap<>();
+		
+		Element mongoConfElement = getSpecificElement("mongo-conf");
+		@SuppressWarnings("unchecked")
+		List<Element> elements = mongoConfElement.elements();
+		for (Element element : elements) {
+			String name = element.getName();
+			String value = element.getText();
+			mongoConfig.put(name, value);
+		}
+		
+		return mongoConfig;
+	}
+	
+	public HashMap<String, String> getGRpcConfig() {
+		HashMap<String, String> gRpcConfig = new HashMap<>();
+		
+		Element gRpcConfigElement = getSpecificElement("rpc-conf");
+		
+		@SuppressWarnings("unchecked")
+		List<Element> elements = gRpcConfigElement.elements();
+		for (Element element : elements) {
+			String name = element.getName();
+			String value = element.getText();
+			gRpcConfig.put(name, value);
+		}
+		
+		return gRpcConfig;
+	}
+	
+	private Element getSpecificElement(String elementName) {
+		@SuppressWarnings("unchecked")
+		List<Element> elements = rootElement.elements();
+		for (Element element : elements) {
+			if (element.getName().equals(elementName)) {
+				return element;
+			}
+		}
+		return null;
 	}
 }
