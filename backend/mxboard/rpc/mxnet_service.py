0060b32b66778b0ad52b9e54d6c30744cf339c08
TerenceWu <terencewuzhenan@126.com>
2017-12-26 17:31:04 +0800

    Update mxboard_service.py
    1. Add log info in every function

--- backend/mxboard/rpc/mxnet_service.py
+++ backend/mxboard/rpc/mxnet_service.py
@@ -21,6 +21,7 @@ class MXNetService(MXNetServiceServicer):
     def createSymbol(self, request, context):
         symbol_name = request.symbol_name
         symbol_desc = request.symbol_desc
+        self._logger.info('mxnet_service has received a new request to create a new symbol with name:%s' % symbol_name)
         if create_symbol(symbol_name, symbol_desc):
             return mxboard_pb2.SymbolCreateState(state_code=SYMBOL_CREATE_STATE_CODES[0],
                                                  state_desc=SYMBOL_CREATE_STATES[0])
@@ -29,8 +30,15 @@ class MXNetService(MXNetServiceServicer):
                                                  state_desc=SYMBOL_CREATE_STATES[1])
 
     def startTask(self, request, context):
-        pass
+        task_id = request.id
+        self._logger.info('mxnet_service has received a new request to start a new task with id:%s' % task_id)
+        # TODO:
+        return mxboard_pb2.TaskState(state_code=0, state_desc='OK_TO_RUN')
 
     def stopTask(self, request, context):
-        pass
+        task_id = request.id
+        self._logger.info('mxnet_service has received a new request to stop the task with id:%s' % task_id)
+        # TODO:
+        return mxboard_pb2.TaskState(state_code=0, state_desc='KILL_SUCCESSFULLY')
+
 
