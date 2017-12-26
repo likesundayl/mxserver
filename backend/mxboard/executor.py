5d3a46b0a304b5615629c004f26edce6dfb456d9
TerenceWu <terencewuzhenan@126.com>
2017-12-26 22:38:24 +0800

    Update mxboard
    1. Update executor_process.py
    2. Add executor.py

---
+++ backend/mxboard/executor.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# ------------------------------
+# Copyright (c) 2017 Terence Wu
+# ------------------------------
+
+
+class Executor(object):
+    def __init__(self):
+        # TODO:
+        pass
+
+    def execute(self):
+        # TODO:
+        pass
--- backend/mxboard/executor_process.py
+++ backend/mxboard/executor_process.py
@@ -3,4 +3,24 @@
 # ------------------------------
 # Copyright (c) 2017 Terence Wu
 # ------------------------------
+from multiprocessing import Process
+from backend.mxboard.log.logger_generator import get_logger
+from backend.mxboard.executor import Executor
 
+
+class ExecutorProcess(Process):
+    def __init__(self, process_id, task_desc):
+        self._process_id = process_id
+        self._logger = get_logger('executor_process_%s' % process_id)
+        self._executor = self._init_executor(task_desc)
+
+    def _init_executor(self, task_desc):
+        # TODO:
+        return Executor()
+
+    def run(self):
+        # TODO: lots of work to do
+        self._executor.execute()
+
+    def terminate(self):
+        super(ExecutorProcess, self).terminate()
