467d6ff31731bc0f9dfd54e500f2e2f8c9c2c82d
TerenceWu <terencewuzhenan@126.com>
2017-12-28 22:34:33 +0800

    Update mongo_connector.py and executor_process.py

--- backend/mxboard/db/mongo_connector.py
+++ backend/mxboard/db/mongo_connector.py
@@ -11,6 +11,25 @@ from backend.mxboard.util.xml_parser import mxboard_mongo_config
 class MongoConnector(object):
     def __init__(self):
         self._client = MongoClient(host=mxboard_mongo_config['host'], port=int(mxboard_mongo_config['port']))
+        self._collection = None
+
+    def insert_one(self, msg):
+        self._collection.insert_one(msg)
+
+    def update_one(self, task_id, msg):
+        pass
 
     def close(self):
         self._client.close()
+
+
+class TaskProgressRecorder(MongoConnector):
+    def __init__(self):
+        super(TaskProgressRecorder, self).__init__()
+        self._collection = self._client.get_database('task-db').get_collection('task-progress')
+
+    def update_one(self, task_id, msg):
+        self._collection.update_one(
+            {'task_id': task_id},
+            {'$set': {}}
+        )
