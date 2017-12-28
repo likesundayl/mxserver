9a16d5c178019e0c37b06dc9fe104e8292a0a833
TerenceWu <terencewuzhenan@126.com>
2017-12-28 15:05:58 +0800

    Add data_loader.py(Generate DataIter instance according to data config)

---
+++ backend/mxboard/io/data_loader.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+# ------------------------------
+# Copyright (c) 2017 Terence Wu
+# ------------------------------
+from mxnet.image.image import ImageIter
+from mxnet.io import DataBatch
+
+
+def load_data_iter_rec(data_config_dict):
+    target = data_config_dict['target']
+    if target == 'train':
+        train_config = data_config_dict['train_config']
+        data_config = train_config['data_config']
+        data_name = data_config['data_name']
+        data_shapes = data_config['data_shape']
+        data_shape_list = [int(data_shape) for data_shape in data_shapes]
+        batch_size = int(data_config['batch_size'])
+        if data_config['shuffle'] == '0':
+            shuffle = True
+        else:
+            shuffle = False
+        train_set_rec = data_config['train_set'][0]
+        if len(data_config['train_set']) == 2:
+            train_set_idx = data_config['train_set'][1]
+        else:
+            train_set_idx = None
+        label_config = train_config['label_config']
+        label_name = label_config['label_name']
+        label_width = int(label_config['label_width'])
+        train_iter = ImageIter(batch_size=batch_size, data_shape=tuple(data_shape_list), label_width=label_width,
+                               path_imgrec=train_set_rec, path_imgidx=train_set_idx, data_name=data_name,
+                               shuffle=shuffle, label_name=label_name)
+        if data_config.get('val_set') is None:
+            return train_iter
+        else:
+            val_set_rec = data_config['val_set'][0]
+            if len(data_config['val_set']) == 2:
+                val_set_idx = data_config['val_set'][1]
+            else:
+                val_set_idx = None
+            val_iter = ImageIter(batch_size=batch_size, data_shape=tuple(data_shape_list), label_width=label_width,
+                                 path_imgrec=val_set_rec, path_imgidx=val_set_idx, data_name=data_name,
+                                 shuffle=shuffle, label_name=label_name)
+            return train_iter, val_iter
+    else:
+        test_config = data_config_dict['test_config']
+        test_datas = test_config['data_config']['datas']
+        # TODO:
+        return None
+
