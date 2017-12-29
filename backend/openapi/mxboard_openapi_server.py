e4b9558adbcba5ce9ccaee5ff75e3596e6f7bec5
TerenceWu <terencewuzhenan@126.com>
2017-12-29 22:20:18 +0800

    Update mxboard_openapi_server.py
    1. Add logger support

--- backend/openapi/mxboard_openapi_server.py
+++ backend/openapi/mxboard_openapi_server.py
@@ -6,28 +6,39 @@
 import grpc
 from flask import Flask, request, jsonify
 from backend.mxboard.proto import mxboard_pb2, mxboard_pb2_grpc
+from backend.mxboard.log.logger_generator import get_logger
 
 # TODO:
 channel = grpc.insecure_channel('127.0.0.1:50051')
 stub = mxboard_pb2_grpc.MXNetServiceStub(channel)
+
+openapi_logger = get_logger('mxboard_openapi')
 
 app = Flask(__name__)
 
 
 @app.route('/train', methods=['POST'])
 def train():
+    task_json = request.json
+    task_id = task_json['task_id']
+    openapi_logger.info('The mxboard_openapi_server receives a request to start a train task with id: %s' % task_id)
     return jsonify(__task_state_2_json(__execute()))
 
 
 @app.route('/test', methods=['POST'])
 def test():
+    task_json = request.json
+    task_id = task_json['task_id']
+    openapi_logger.info('The mxboard_openapi_server receives a request to start a test task with id: %s' % task_id)
     return jsonify(__execute())
 
 
 @app.route('/stop', methods=['POST'])
 def stop():
+    task_id = request.json['task_id']
+    openapi_logger.info('The mxboard_openapi_server receives a request to stop a task with id: %s' % task_id)
     return jsonify(
-        __task_state_2_json(stub.stopTask(mxboard_pb2.TaskId(id=__get_stop_task_id()))))
+        __task_state_2_json(stub.stopTask(mxboard_pb2.TaskId(id=task_id))))
 
 
 def __execute():
@@ -38,12 +49,6 @@ def __execute():
     return task_state
 
 
-def __get_stop_task_id():
-    task_id = request.json['task_id']
-    # TODO: some check work
-    return task_id
-
-
 def __task_state_2_json(task_state):
     state_id = task_state.id
     state_code = task_state.state_code
@@ -52,4 +57,8 @@ def __task_state_2_json(task_state):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    try:
+        openapi_logger.info('The mxboard_openapi_server has been started')
+        app.run(host='0.0.0.0')
+    except KeyboardInterrupt:
+        openapi_logger.warn('The mxboard_openapi_server has been stopped manually!')
