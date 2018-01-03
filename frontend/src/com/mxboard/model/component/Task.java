d5376a115ceeb3329b1714f2e5cf7f7d5580c612
TerenceWu <terencewuzhenan@126.com>
2018-01-03 13:46:27 +0800

    Update frontend
    1. Finish Task.java

--- frontend/src/com/mxboard/model/component/Task.java
+++ frontend/src/com/mxboard/model/component/Task.java
@@ -13,9 +13,33 @@ import com.mxboard.model.component.net.Symbol;
  * @version 1.0
  */
 public class Task {
-	private int id;
+	private String id;
 	private boolean forTraining = true;
 	private Symbol net;
 	private TrainParam trainParam;
 	private TestParam testParam;
+	
+	public Task(String id, boolean forTraining, Symbol net, TrainParam trainParam, TestParam testParam) {
+		this.id = id;
+		this.forTraining = forTraining;
+		this.net = net;
+		this.trainParam = trainParam;
+		this.testParam = testParam;
+	}
+	
+	public String toJSON() {
+		StringBuilder builder = new StringBuilder("{");
+		
+		builder.append("\"task_id\": \"")
+				.append(id).append("\", \"for_training\": \"")
+				.append(forTraining).append("\", \"net\": ")
+				.append(net.toJSON()).append(", ");
+		if(forTraining) {
+			builder.append("\"train_param\": ").append(trainParam.toJSON()).append("}");
+		} else {
+			builder.append("\"test_param\": ").append(testParam.toJSON()).append("}");
+		}
+		
+		return builder.toString();
+	}
 }
