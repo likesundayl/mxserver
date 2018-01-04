14a7f12ee8ce2a9db2bc272cf8b7ed0c19707724
TerenceWu <terencewuzhenan@126.com>
2018-01-04 10:00:04 +0800

    Update executor.py
    1. Add some sub classes for Executor to finish different jobs

--- backend/mxboard/core/executor.py
+++ backend/mxboard/core/executor.py
@@ -3,10 +3,12 @@
 # ------------------------------
 # Copyright (c) 2017 Terence Wu
 # ------------------------------
+import sys
 from backend.mxboard.util.xml_parser import mxboard_mxnet_config
 from mxnet.module import Module
 
 RCNN_PATH = mxboard_mxnet_config['rcnn-path']
+sys.path.append(RCNN_PATH)
 
 
 class Executor(Module):
@@ -20,5 +22,69 @@ class Executor(Module):
             self._val_iter = None
 
     def execute(self):
-        # TODO:
+        pass
+
+    @staticmethod
+    def create_executor(for_training, exec_type, **kwargs):
+        if for_training:
+            if exec_type == 'classify':
+                return Executor._registered_trainer['classifytrainer'](**kwargs)
+            elif exec_type == 'detection':
+                return Executor._registered_trainer['rcnntrainer'](**kwargs)
+        else:
+            if exec_type == 'classify':
+                return Executor._registered_predictor['classifier'](**kwargs)
+            elif exec_type == 'detection':
+                return Executor._registered_trainer['objectdetector'](**kwargs)
+
+    _registered_predictor = {}
+    _registered_trainer = {}
+
+    @staticmethod
+    def register(kclass, for_training):
+        assert (isinstance(kclass, type))
+        name = kclass.__name__.lower()
+        if for_training:
+            if name not in Executor._registered_trainer:
+                Executor._registered_trainer[name] = kclass
+        else:
+            if name not in Executor._registered_predictor:
+                Executor._registered_predictor[name] = kclass
+        return kclass
+
+
+register = Executor.register
+
+
+class Predictor(Executor):
+    def __init__(self):
+        pass
+
+
+@register(for_training=False)
+class Classifier(Predictor):
+    def __init__(self):
+        pass
+
+
+@register(for_training=False)
+class ObjectDetector(Predictor):
+    def __init__(self):
+        pass
+
+
+class Trainer(Executor):
+    def __init__(self):
+        pass
+
+
+@register(for_training=True)
+class ClassifyTrainer(Trainer):
+    def __init__(self):
+        pass
+
+
+@register(for_training=True)
+class RCNNTrainer(Trainer):
+    def __init__(self):
         pass
