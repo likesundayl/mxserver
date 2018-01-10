4d30defe75f61923668d1e808ceab6c1567b4feb
TerenceWu <terencewuzhenan@126.com>
2018-01-10 13:47:15 +0800

    Update mxboard.proto and its generated code

--- backend/mxboard/proto/mxboard_pb2.py
+++ backend/mxboard/proto/mxboard_pb2.py
@@ -19,7 +19,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='mxboard.proto',
   package='',
   syntax='proto2',
-  serialized_pb=_b('\n\rmxboard.proto\"N\n\x0fSymbolParameter\x12\x11\n\tsymbol_id\x18\x01 \x02(\t\x12\x13\n\x0bsymbol_name\x18\x02 \x02(\t\x12\x13\n\x0bsymbol_desc\x18\x03 \x02(\t\"]\n\x11SymbolCreateState\x12\x11\n\tsymbol_id\x18\x01 \x02(\t\x12\x15\n\nstate_code\x18\x02 \x02(\x05:\x01\x30\x12\x1e\n\nstate_desc\x18\x03 \x02(\t:\nSUCCESSFUL\"7\n\rTaskParameter\x12\x13\n\x02id\x18\x01 \x02(\x0b\x32\x07.TaskId\x12\x11\n\ttask_desc\x18\x02 \x02(\t\"\x19\n\x06TaskId\x12\x0f\n\x07task_id\x18\x01 \x02(\t\"R\n\tTaskState\x12\x0f\n\x07task_id\x18\x01 \x02(\t\x12\x15\n\nstate_code\x18\x02 \x02(\x05:\x01\x30\x12\x1d\n\nstate_desc\x18\x03 \x02(\t:\tOK_TO_RUN\"c\n\x0fRecordIORequest\x12\x12\n\nimage_root\x18\x01 \x02(\t\x12\x16\n\x0elabel_lst_path\x18\x02 \x02(\t\x12\x15\n\x07shuffle\x18\x03 \x02(\x08:\x04true\x12\r\n\x05shape\x18\x04 \x03(\x05\"L\n\x13RecordIOCreateState\x12\x15\n\nstate_code\x18\x01 \x02(\x05:\x01\x30\x12\x1e\n\nstate_desc\x18\x02 \x02(\t:\nSUCCESSFUL2\xd8\x01\n\x0cMXNetService\x12\x42\n\x16\x63reateClsRecordIOFiles\x12\x10.RecordIORequest\x1a\x14.RecordIOCreateState\"\x00\x12\x36\n\x0c\x63reateSymbol\x12\x10.SymbolParameter\x1a\x12.SymbolCreateState\"\x00\x12)\n\tstartTask\x12\x0e.TaskParameter\x1a\n.TaskState\"\x00\x12!\n\x08stopTask\x12\x07.TaskId\x1a\n.TaskState\"\x00\x42\x19\n\x17\x63om.mxboard.service.rpc')
+  serialized_pb=_b('\n\rmxboard.proto\"N\n\x0fSymbolParameter\x12\x11\n\tsymbol_id\x18\x01 \x02(\t\x12\x13\n\x0bsymbol_name\x18\x02 \x02(\t\x12\x13\n\x0bsymbol_desc\x18\x03 \x02(\t\"]\n\x11SymbolCreateState\x12\x11\n\tsymbol_id\x18\x01 \x02(\t\x12\x15\n\nstate_code\x18\x02 \x02(\x05:\x01\x30\x12\x1e\n\nstate_desc\x18\x03 \x02(\t:\nSUCCESSFUL\"7\n\rTaskParameter\x12\x13\n\x02id\x18\x01 \x02(\x0b\x32\x07.TaskId\x12\x11\n\ttask_desc\x18\x02 \x02(\t\"\x19\n\x06TaskId\x12\x0f\n\x07task_id\x18\x01 \x02(\t\"R\n\tTaskState\x12\x0f\n\x07task_id\x18\x01 \x02(\t\x12\x15\n\nstate_code\x18\x02 \x02(\x05:\x01\x30\x12\x1d\n\nstate_desc\x18\x03 \x02(\t:\tOK_TO_RUN\"\x9a\x01\n\x0fRecordIORequest\x12\x12\n\nimage_root\x18\x01 \x02(\t\x12\x16\n\x0elabel_lst_path\x18\x02 \x02(\t\x12,\n\x06\x63onfig\x18\x04 \x03(\x0b\x32\x1c.RecordIORequest.ConfigEntry\x1a-\n\x0b\x43onfigEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\"L\n\x13RecordIOCreateState\x12\x15\n\nstate_code\x18\x01 \x02(\x05:\x01\x30\x12\x1e\n\nstate_desc\x18\x02 \x02(\t:\nSUCCESSFUL2\xd8\x01\n\x0cMXNetService\x12\x42\n\x16\x63reateClsRecordIOFiles\x12\x10.RecordIORequest\x1a\x14.RecordIOCreateState\"\x00\x12\x36\n\x0c\x63reateSymbol\x12\x10.SymbolParameter\x1a\x12.SymbolCreateState\"\x00\x12)\n\tstartTask\x12\x0e.TaskParameter\x1a\n.TaskState\"\x00\x12!\n\x08stopTask\x12\x07.TaskId\x1a\n.TaskState\"\x00\x42\x19\n\x17\x63om.mxboard.service.rpc')
 )
 
 
@@ -229,6 +229,43 @@ _TASKSTATE = _descriptor.Descriptor(
 )
 
 
+_RECORDIOREQUEST_CONFIGENTRY = _descriptor.Descriptor(
+  name='ConfigEntry',
+  full_name='RecordIORequest.ConfigEntry',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='key', full_name='RecordIORequest.ConfigEntry.key', index=0,
+      number=1, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='value', full_name='RecordIORequest.ConfigEntry.value', index=1,
+      number=2, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  options=_descriptor._ParseOptions(descriptor_pb2.MessageOptions(), _b('8\001')),
+  is_extendable=False,
+  syntax='proto2',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=470,
+  serialized_end=515,
+)
+
 _RECORDIOREQUEST = _descriptor.Descriptor(
   name='RecordIORequest',
   full_name='RecordIORequest',
@@ -251,15 +288,8 @@ _RECORDIOREQUEST = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='shuffle', full_name='RecordIORequest.shuffle', index=2,
-      number=3, type=8, cpp_type=7, label=2,
-      has_default_value=True, default_value=True,
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      options=None, file=DESCRIPTOR),
-    _descriptor.FieldDescriptor(
-      name='shape', full_name='RecordIORequest.shape', index=3,
-      number=4, type=5, cpp_type=1, label=3,
+      name='config', full_name='RecordIORequest.config', index=2,
+      number=4, type=11, cpp_type=10, label=3,
       has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -267,7 +297,7 @@ _RECORDIOREQUEST = _descriptor.Descriptor(
   ],
   extensions=[
   ],
-  nested_types=[],
+  nested_types=[_RECORDIOREQUEST_CONFIGENTRY, ],
   enum_types=[
   ],
   options=None,
@@ -276,8 +306,8 @@ _RECORDIOREQUEST = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=360,
-  serialized_end=459,
+  serialized_start=361,
+  serialized_end=515,
 )
 
 
@@ -314,11 +344,13 @@ _RECORDIOCREATESTATE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=461,
-  serialized_end=537,
+  serialized_start=517,
+  serialized_end=593,
 )
 
 _TASKPARAMETER.fields_by_name['id'].message_type = _TASKID
+_RECORDIOREQUEST_CONFIGENTRY.containing_type = _RECORDIOREQUEST
+_RECORDIOREQUEST.fields_by_name['config'].message_type = _RECORDIOREQUEST_CONFIGENTRY
 DESCRIPTOR.message_types_by_name['SymbolParameter'] = _SYMBOLPARAMETER
 DESCRIPTOR.message_types_by_name['SymbolCreateState'] = _SYMBOLCREATESTATE
 DESCRIPTOR.message_types_by_name['TaskParameter'] = _TASKPARAMETER
@@ -364,11 +396,19 @@ TaskState = _reflection.GeneratedProtocolMessageType('TaskState', (_message.Mess
 _sym_db.RegisterMessage(TaskState)
 
 RecordIORequest = _reflection.GeneratedProtocolMessageType('RecordIORequest', (_message.Message,), dict(
+
+  ConfigEntry = _reflection.GeneratedProtocolMessageType('ConfigEntry', (_message.Message,), dict(
+    DESCRIPTOR = _RECORDIOREQUEST_CONFIGENTRY,
+    __module__ = 'mxboard_pb2'
+    # @@protoc_insertion_point(class_scope:RecordIORequest.ConfigEntry)
+    ))
+  ,
   DESCRIPTOR = _RECORDIOREQUEST,
   __module__ = 'mxboard_pb2'
   # @@protoc_insertion_point(class_scope:RecordIORequest)
   ))
 _sym_db.RegisterMessage(RecordIORequest)
+_sym_db.RegisterMessage(RecordIORequest.ConfigEntry)
 
 RecordIOCreateState = _reflection.GeneratedProtocolMessageType('RecordIOCreateState', (_message.Message,), dict(
   DESCRIPTOR = _RECORDIOCREATESTATE,
@@ -380,6 +420,8 @@ _sym_db.RegisterMessage(RecordIOCreateState)
 
 DESCRIPTOR.has_options = True
 DESCRIPTOR._options = _descriptor._ParseOptions(descriptor_pb2.FileOptions(), _b('\n\027com.mxboard.service.rpc'))
+_RECORDIOREQUEST_CONFIGENTRY.has_options = True
+_RECORDIOREQUEST_CONFIGENTRY._options = _descriptor._ParseOptions(descriptor_pb2.MessageOptions(), _b('8\001'))
 
 _MXNETSERVICE = _descriptor.ServiceDescriptor(
   name='MXNetService',
@@ -387,8 +429,8 @@ _MXNETSERVICE = _descriptor.ServiceDescriptor(
   file=DESCRIPTOR,
   index=0,
   options=None,
-  serialized_start=540,
-  serialized_end=756,
+  serialized_start=596,
+  serialized_end=812,
   methods=[
   _descriptor.MethodDescriptor(
     name='createClsRecordIOFiles',
