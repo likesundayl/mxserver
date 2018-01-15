b3561499c40dcaae74318328e9aa42bf1eb1b829
TerenceWu <terencewuzhenan@126.com>
2018-01-15 13:28:59 +0800

    Add faster_rcnn.py in package model_zoo

---
+++ backend/mxboard/core/model_zoo/faster_rcnn.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# @Author: Terence Wu
+# @Time: 15/01/18 下午 01:27
+"""
+faster-rcnn
+"""
+
+
+def faster_rcnn(num_classes, for_training, backbone='vgg', num_layers='101', **kwargs):
+    pass
