d1d3c7e673302ff5d5af31f0d4a7b3206c9d0fc4
TerenceWu <terencewuzhenan@126.com>
2018-02-07 16:05:33 +0800

    Update mxserver

--- worker/mx/gpu/gpu_entity.py
+++ worker/mx/gpu/gpu_entity.py
@@ -2,3 +2,25 @@
 
 # @Author: Terence Wu
 # @Time: 07/02/18 下午 02:42
+from ctypes import c_int, c_longlong, Structure
+from worker.mx.util.xml_parser import mxserver_gpu_config
+
+LOCAL_GPU_NUM = mxserver_gpu_config['gpu-num']
+
+
+class GpuMemory(Structure):
+    _fields_ = [
+        ('total', c_longlong),
+        ('used', c_longlong),
+        ('free', c_longlong)
+    ]
+
+
+GPU_ARRAY = LOCAL_GPU_NUM * GpuMemory
+
+
+class GpuInfos(Structure):
+    __fields_ = [
+        ('gpu_num', c_int),
+        ('gpus', GPU_ARRAY)
+    ]
