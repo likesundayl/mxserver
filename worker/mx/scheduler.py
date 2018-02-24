d5a9b1c4322934cb5bcbbaf536203bb0c902550e
TerenceWu <terencewuzhenan@126.com>
2018-02-24 11:06:05 +0800

    First commit after Spring Festival, back to work
    1. Add scheduler.py

---
+++ worker/mx/scheduler.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# @Author: Terence Wu
+# @Time: 24/02/18 上午 10:55
+"""
+Deep Learning Task Scheduler
+"""
+
+
+class Scheduler(object):
+    def __init__(self):
+        pass
+
+
+class NormalQueueScheduler(Scheduler):
+    def __init__(self):
+        super(NormalQueueScheduler, self).__init__()
+        pass
+
+
+class PriorityQueueScheduler(Scheduler):
+    def __init__(self):
+        super(PriorityQueueScheduler, self).__init__()
+        pass
