766564778635f9cc24b7041f19a76c99cc885d07
TerenceWu <terencewuzhenan@126.com>
2018-02-27 13:58:58 +0800

    Update test_resources.py and test_stop_request.py

--- test/test_resources.py
+++ test/test_resources.py
@@ -11,7 +11,68 @@ STOP_TEST_URL = TEST_BASE_URL + '/stop'
 QUERY_GPU_TEST_URL = TEST_BASE_URL + '/gpu'
 
 TRAIN_REQUEST_JSON = {
-
+  "net": {
+    "type": "built_in",
+    "name": "alexnet",
+    "config": {
+        "num_classes": "10"
+    }
+  },
+  "for_training": "0",
+  "target": "classify",
+  "context": [
+    {"device_name": "gpu", "device_id": "0"}
+  ],
+  "eval_metrics": ["acc"],
+  "train_param": {
+    "begin_epoch": "0",
+    "num_epoch": "250",
+    "kvstore": "local",
+    "save_prefix": "alex",
+    "save_period": "50",
+    "resume_config": {
+      "is_resume": "1",
+      "ckp": {}
+    },
+    "data_param": {
+      "batch_size": "32",
+      "shuffle": "0",
+      "name": "data",
+      "shapes": ["3", "128", "128"],
+      "file_path": {
+        "train_rec": "train.rec",
+        "train_idx": "train.idx",
+        "val_rec": "val.rec",
+      }
+    },
+    "label_param": {
+      "name": "softmax_label",
+      "shapes": ["1"]
+    },
+    "lr_scheduler": {
+      "type": "Factor",
+      "lr_scheduler_config": {
+        "step": "1000",
+        "factor": "0.1",
+        "stop_factor_lr": "0.001"
+      }
+    },
+    "optimizer": {
+      "type": "sgd",
+      "opt_config": {
+        "base_lr": "0.1",
+        "wd": "0.0005",
+        "momentum": "0.9"
+      }
+    },
+    "initializer": {
+      "type": "normal",
+      "init_config": {
+        "sigma": "0.25"
+      }
+    }
+  },
+  "test_param": {}
 }
 
 TEST_REQUEST_JSON = {
@@ -19,5 +80,5 @@ TEST_REQUEST_JSON = {
 }
 
 STOP_REQUEST_JSON = {
-
+    "task_id": "19700101"
 }
--- test/test_stop_request.py
+++ test/test_stop_request.py
@@ -2,3 +2,20 @@
 
 # @Author: Terence Wu
 # @Time: 26/02/18 上午 11:37
+from requests import post
+from test_resources import STOP_TEST_URL, STOP_REQUEST_JSON
+from util.logger_generator import get_logger
+from util.exception_handler import exception_msg
+
+
+if __name__ == '__main__':
+    logger = get_logger('test_stop_request')
+    logger.info('Begin to test API for deep learning training')
+    logger.info('Begin to send request to url: %s' % STOP_TEST_URL)
+    try:
+        response = post(url=STOP_TEST_URL, json=STOP_REQUEST_JSON)
+        logger.info('Receive a response')
+        logger.info('Response\'s status code: %s' % response.status_code)
+        logger.info('Response\'s content: %s' % response.content)
+    except StandardError as e:
+        logger.error('Fail! Error message: %s\n' % exception_msg(e))
